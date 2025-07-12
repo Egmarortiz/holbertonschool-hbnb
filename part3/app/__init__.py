6758f43b4c9b66285b46c6683429abebdb420e55
Egmarortiz <egmarocasio9@gmail.com>
2025-07-11 21:55:22 -0400

    circular import fix

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
 from flask_bcrypt import Bcrypt
 import config
 
@@ -13,6 +9,10 @@ def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
 
